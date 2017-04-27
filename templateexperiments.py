cd88dd729ddb07c7978ecc464112a2d5bd4c6d4b
Colin Quirk <cquirk@uchicago.edu>
2017-04-27 16:41:05 -0500

    open window method started, monitor setup moved to init

--- templateexperiments.py
+++ templateexperiments.py
@@ -1,18 +1,36 @@
 """Contains all basic experiment classes"""
 
 import psychopy.gui
+import psychopy.monitors
+import psychopy.visual
 
 
 class BaseExperiment(object):
     """This is the BaseExperiment class"""
-    def __init__(self, data_fields):
-        """Creates a new BaseExperiment object
+    def __init__(self, experiment_name, data_fields,
+                 monitor_name="Experiment Monitor", monitor_width=53,
+                 monitor_distance=70,):
+        """Creates a new BaseExperiment object.
 
         Parameters:
         data_fields -- list of strings containing the data fields to be stored
+        experiment_name -- A title for the experiment that also defines the
+            filename the experiment info from the dialog box is saved to
+        monitor_width -- The length of the display monitor in cm
+        monitor_distance -- The distance the participant sits
+            from the monitor in cm
         """
-        experiment_data = []
+        self.experiment_data = []
+        self.experiment_window = None  # Set in open_window()
+
+        self.experiment_name = experiment_name
         self.data_fields = data_fields
+        self.monitor_name = monitor_name
+        self.monitor_width = monitor_width
+
+        self.experiment_monitor = psychopy.monitors.Monitor(
+            self.monitor_name, width=monitor_width, distance=monitor_distance
+        )
 
     def display_instructions(self, instruction_text=""):
         """Takes a string with instructions as input and prints them to the screen.
@@ -32,19 +50,19 @@ class BaseExperiment(object):
         additional_fields_dict -- An optional dictionary containing more
             fields for the dialog box and output dictionary
         """
-        experiment_info = {'Subject Number': '0',
-                           'Age': '0',
-                           'Experimenter Initials': 'CQ',
-                           'Unique Subject Identifier': '000000',
-                           'Pickle File': '',
-                           }
+        self.experiment_info = {'Subject Number': '0',
+                                'Age': '0',
+                                'Experimenter Initials': 'CQ',
+                                'Unique Subject Identifier': '000000',
+                                'Pickle File': '',
+                                }
 
         if additional_fields_dict is not None:
-            experiment_info.update(additional_fields_dict)
+            self.experiment_info.update(additional_fields_dict)
 
         # Modifies experiment_info dict directly
         psychopy.gui.DlgFromDict(
-            experiment_info, title="Experiment Info",
+            experiment_info, title=self.experiment_name,
             order=['Subject Number',
                    'Age',
                    'Experimenter Initials',
@@ -54,18 +72,24 @@ class BaseExperiment(object):
             tip={'Unique Subject Identifier': 'From the cronus log',
                  'Pickle File': 'Load if restarting from a crash',
                  }
-         )
+        )
 
         return experiment_info
 
-    def open_csv_data_file(self):
+    def __open_csv_data_file(self):
         pass
 
     def open_window(self):
         """Opens the psychopy window."""
-        pass
+        self.experiment_window = psychopy.visual.Window(
+            monitor=self.experiment_monitor, fullscr=True, allowGUI=False,
+            winType='pyglet', units='deg', screen=0)  # check screen=0 is correct
+                                                      # check if waitBlanking is important
 
     def save_data_to_csv(self):
+        pass
+
+    def save_experiment_info(self):
         pass
 
     def save_experiment_pickle(self):
@@ -79,4 +103,3 @@ class BaseExperiment(object):
             Only keys that are included in data_fields are extended
         """
         pass
-
