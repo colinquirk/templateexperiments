53dbd3dc4f15b13fafbf3ec8cce9daddedbd9227
Colin Quirk <cquirk@uchicago.edu>
2019-12-08 21:46:37 -0600

    test convert_color_value

--- template/test_template.py
+++ template/test_template.py
@@ -1,2 +1,29 @@
+import unittest
 import template
 
+
+class TestTemplateMethods(unittest.TestCase):
+    def setUp(self):
+        self.basic_template = template.BaseExperiment(experiment_name='test_name',
+                                                      data_fields=['1', '2', '3'])
+
+    def assertListAlmostEqual(self, list1, list2, tol=1e-5):
+        self.assertEqual(len(list1), len(list2))
+        for a, b in zip(list1, list2):
+            self.assertAlmostEqual(a, b, tol)
+
+    def test_convert_color_value(self):
+        self.assertListAlmostEqual(template.convert_color_value([255, 255, 255]),
+                                   [1.0, 1.0, 1.0])  # White
+        self.assertListAlmostEqual(template.convert_color_value([128, 128, 128]),
+                                   [0.0, 0.0, 0.0])  # Grey
+        self.assertListAlmostEqual(template.convert_color_value([0, 0, 0]),
+                                   [-1.0, -1.0, -1.0])  # Black
+        self.assertListAlmostEqual(template.convert_color_value([255, 0, 0]),
+                                   [1.0, -1.0, -1.0])  # Red
+        self.assertListAlmostEqual(template.convert_color_value([188, 108, 14]),
+                                   [0.47, -0.15, -0.89])  # Brown
+
+
+if __name__ == '__main__':
+    unittest.main()
